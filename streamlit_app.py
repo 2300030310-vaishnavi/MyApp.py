a8a0c09b46ca9a66c3bc23ccd22ce9fabafd19ff
Karuturi Vaishnavi <vaishnavikaruturi2006@gmail.com>
2026-09-25 20:38:00 +0530

    Implement task tracker features in Streamlit app
    Added functionality for task input, subject selection, and priority selection in the app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,24 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title("Student Study & Task Tracker")
 
-st.write('Hello world!')
+task = st.text_input("Enter your task")
+
+subject = st.selectbox(
+    "Select subject",
+    ["Python", "DBMS", "Image Processing", "AIML"]
+)
+
+priority = st.selectbox(
+    "Select priority",
+    ["High", "Medium", "Low"]
+)
+
+if st.button("Add Task"):
+    if task:
+        st.success("Task added successfully!")
+        st.write("Task:", task)
+        st.write("Subject:", subject)
+        st.write("Priority:", priority)
+    else:
+        st.warning("Please enter a task.")
